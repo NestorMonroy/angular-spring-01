6b1749f8c962b9c597f72e3143a9f255e622d9f4
NestorMonroy <nestor.monroy.90@gmail.com>
2023-01-20 17:41:11 -0600

    [update] form html

--- angular/clientes-app/src/app/clientes/form.component.ts
+++ angular/clientes-app/src/app/clientes/form.component.ts
@@ -54,6 +54,8 @@ export class FormComponent {
   };
 
   update():void{
+    console.log(this.cliente);
+    this.cliente.facturas = null;
     this.clienteService.update(this.cliente)
     .subscribe(json => {
       this.router.navigate(['/clientes'])
